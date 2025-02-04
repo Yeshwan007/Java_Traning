809b9a760bec433a7864d69ddddde06fbd082c70
puttu yaswanth <puttuyaswanth0123@gmail.com>
2025-02-04 11:43:39 +0530

    Initial commit

--- day2/Evenodd.java
+++ day2/Evenodd.java
@@ -1,8 +1,11 @@
 public class Evenodd {
     public static void main(String[] args) {
-    int n=3;
-    if(n%2==)
-
+        int n = 3;
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
     }
-    
+
 }
